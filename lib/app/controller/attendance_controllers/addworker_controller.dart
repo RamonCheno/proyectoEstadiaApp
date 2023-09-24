aa53d5561f64a5c7aee0617adb23898ab508150e
Ramon Cheno <francisco.cheno.ocano@gmail.com>
2023-09-23 22:11:12 -0700

    controlador worker actualizado

--- lib/app/controller/attendance_controllers/addworker_controller.dart
+++
@@ -1,8 +0,0 @@
-import 'package:control_asistencia_app/app/model/api/http_request.dart';
-import 'package:control_asistencia_app/app/model/worker_model.dart';
-
-class AddWorkerController {
-  Future<String> addWorker(WorkerModel workerModel) async {
-    return "";
-  }
-}
--- lib/app/view/screen/admin_screens/addworker_screen.dart
+++ lib/app/view/screen/admin_screens/addworker_screen.dart
@@ -1,3 +1,5 @@
+import 'package:control_asistencia_app/app/controller/attendance_controllers/worker_controller.dart';
+import 'package:control_asistencia_app/app/model/worker_model.dart';
 import 'package:control_asistencia_app/app/view/screen/admin_screens/listworker_screen.dart';
 import 'package:control_asistencia_app/app/view/widget/customtextformfield_widget.dart';
 import 'package:flutter/material.dart';
@@ -18,6 +20,7 @@ class _AddWorkerScreenState extends State<AddWorkerScreen> {
   late TextEditingController _conIMSSWorker;
   late TextEditingController _conworkerPosition;
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  late WorkerController workerController;
 
   void addWorker() async {
     final FormState? form = _formKey.currentState;
@@ -28,6 +31,59 @@ class _AddWorkerScreenState extends State<AddWorkerScreen> {
     String curpWorker = _conCurpWorker.text.toUpperCase();
     String numIMSSWorker = _conIMSSWorker.text;
     String workerPosition = _conworkerPosition.text;
+
+    if (form != null) {
+      if (form.validate()) {
+        WorkerModel workerModel = WorkerModel(
+          numTrabajador: int.parse(numWorker),
+          nombre: nameWorker.trim(),
+          curp: curpWorker.trim(),
+          rfc: rfcWorker.trim(),
+          numImss: int.parse(numIMSSWorker),
+          puesto: workerPosition.trim(),
+        );
+        String respuesta = await workerController.addWorker(workerModel).then(
+          (workerController) {
+            return workerController;
+          },
+        );
+        if (!mounted) return;
+        showDialog(
+          context: context,
+          builder: (context) {
+            Future.delayed(const Duration(seconds: 3));
+            return AlertDialog(
+              content: Text(respuesta),
+            );
+          },
+        );
+        Navigator.of(context).pop();
+        Navigator.of(context).pushReplacementNamed(ListWorkerScreen.route);
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    workerController = WorkerController();
+    _conNumWorker = TextEditingController();
+    _conNameWorker = TextEditingController();
+    _conRFCWorker = TextEditingController();
+    _conCurpWorker = TextEditingController();
+    _conIMSSWorker = TextEditingController();
+    _conworkerPosition = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _conNumWorker.dispose();
+    _conNameWorker.dispose();
+    _conRFCWorker.dispose();
+    _conCurpWorker.dispose();
+    _conIMSSWorker.dispose();
+    _conworkerPosition.dispose();
   }
 
   @override
@@ -40,123 +96,120 @@ class _AddWorkerScreenState extends State<AddWorkerScreen> {
       ),
       backgroundColor: const Color(0xffEBEBEB),
       body: Form(
+          key: _formKey,
           child: SingleChildScrollView(
-        child: Column(
-          children: [
-            const Padding(
-              padding: EdgeInsets.symmetric(vertical: 10),
-              child: Column(
-                children: [
-                  Material(
-                    color: Color(0xffE1E1E1),
-                    shape: CircleBorder(),
-                    child: Icon(
-                      Icons.person_outline,
-                      size: 70,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            CustomTextFormWidget(
-              controller: _conNumWorker,
-              hintName: "Num Trabajador",
-              icon: Icons.numbers,
-              isObscureText: false,
-              inputType: TextInputType.number,
-              action: TextInputAction.next,
-              soloLeer: false,
-            ),
-            CustomTextFormWidget(
-              controller: _conNameWorker,
-              hintName: "Nombre Trabajador",
-              icon: Icons.person_outline,
-              isObscureText: false,
-              inputType: TextInputType.text,
-              action: TextInputAction.next,
-              soloLeer: false,
-            ),
-            CustomTextFormWidget(
-              controller: _conRFCWorker,
-              hintName: "RFC",
-              icon: Icons.person_outline,
-              isObscureText: false,
-              inputType: TextInputType.text,
-              action: TextInputAction.next,
-              soloLeer: false,
-            ),
-            CustomTextFormWidget(
-              controller: _conCurpWorker,
-              hintName: "Curp",
-              icon: Icons.person_outline,
-              isObscureText: false,
-              inputType: TextInputType.text,
-              action: TextInputAction.next,
-              soloLeer: false,
-            ),
-            CustomTextFormWidget(
-              controller: _conIMSSWorker,
-              hintName: "Num Imss",
-              icon: Icons.numbers,
-              isObscureText: false,
-              inputType: TextInputType.number,
-              action: TextInputAction.next,
-              soloLeer: false,
-            ),
-            CustomTextFormWidget(
-              controller: _conworkerPosition,
-              hintName: "Puesto",
-              icon: Icons.person_outline,
-              isObscureText: false,
-              inputType: TextInputType.text,
-              action: TextInputAction.done,
-              soloLeer: false,
-            ),
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 10),
-              child: Center(
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    elevation: 1,
-                    padding: const EdgeInsets.symmetric(horizontal: 24),
-                    textStyle: const TextStyle(fontSize: 18),
-                    backgroundColor: const Color(0xFFD9D9D9),
-                    foregroundColor: const Color(0xFF000000),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                  ),
-                  onPressed: () {
-                    showDialog(
-                      context: context,
-                      builder: (context) {
-                        return AlertDialog(
-                          title: const Text('Trabajador agregado'),
-                          content: const Text(
-                              'Trabajador (Nombre) fue agregado correctamente'),
-                          actions: [
-                            TextButton(
-                              child: const Text('Aceptar'),
-                              onPressed: () async {
-                                Navigator.of(context).pop();
-                                Navigator.of(context).pushReplacementNamed(
-                                    ListWorkerScreen.route);
-                              },
-                            ),
-                          ],
-                        );
-                      },
-                    );
-                  },
-                  child: const Text(
-                    'Registrar',
+            child: Column(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  child: Column(
+                    children: [
+                      Material(
+                        color: Color(0xffE1E1E1),
+                        shape: CircleBorder(),
+                        child: Icon(
+                          Icons.person_outline,
+                          size: 70,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ),
+                CustomTextFormWidget(
+                  controller: _conNumWorker,
+                  hintName: "Num Trabajador",
+                  icon: Icons.numbers,
+                  isObscureText: false,
+                  inputType: TextInputType.number,
+                  action: TextInputAction.next,
+                  soloLeer: false,
+                ),
+                CustomTextFormWidget(
+                  controller: _conNameWorker,
+                  hintName: "Nombre Trabajador",
+                  icon: Icons.person_outline,
+                  isObscureText: false,
+                  inputType: TextInputType.text,
+                  action: TextInputAction.next,
+                  soloLeer: false,
+                ),
+                CustomTextFormWidget(
+                  controller: _conRFCWorker,
+                  hintName: "RFC",
+                  icon: Icons.person_outline,
+                  isObscureText: false,
+                  inputType: TextInputType.text,
+                  action: TextInputAction.next,
+                  soloLeer: false,
+                ),
+                CustomTextFormWidget(
+                  controller: _conCurpWorker,
+                  hintName: "Curp",
+                  icon: Icons.person_outline,
+                  isObscureText: false,
+                  inputType: TextInputType.text,
+                  action: TextInputAction.next,
+                  soloLeer: false,
+                ),
+                CustomTextFormWidget(
+                  controller: _conIMSSWorker,
+                  hintName: "Num Imss",
+                  icon: Icons.numbers,
+                  isObscureText: false,
+                  inputType: TextInputType.number,
+                  action: TextInputAction.next,
+                  soloLeer: false,
+                ),
+                CustomTextFormWidget(
+                  controller: _conworkerPosition,
+                  hintName: "Puesto",
+                  icon: Icons.person_outline,
+                  isObscureText: false,
+                  inputType: TextInputType.text,
+                  action: TextInputAction.done,
+                  soloLeer: false,
+                ),
+                Container(
+                  margin: const EdgeInsets.symmetric(vertical: 10),
+                  child: Center(
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        elevation: 1,
+                        padding: const EdgeInsets.symmetric(horizontal: 24),
+                        textStyle: const TextStyle(fontSize: 18),
+                        backgroundColor: const Color(0xFFD9D9D9),
+                        foregroundColor: const Color(0xFF000000),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                      onPressed: () {
+                        showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              title: const Text('Trabajador agregado'),
+                              content: const Text(
+                                  'Trabajador (Nombre) fue agregado correctamente'),
+                              actions: [
+                                TextButton(
+                                  onPressed: addWorker,
+                                  child: const Text('Aceptar'),
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      },
+                      child: const Text(
+                        'Registrar',
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-      )),
+          )),
     );
   }
 }
